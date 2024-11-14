599144d3595004c36ad286ac9ac047512f70fd75
Ahmad Allaou <120275309+ucbahallaou@users.noreply.github.com>
2024-11-14 13:36:47 -0600

    fixing stuff

--- main.py
+++ main.py
@@ -124,17 +124,17 @@ else:
             3. Enter the patient's responsibility percentage in the 'Enter % Patient Responsibility' field.
             4. Click 'Add CPT Code' to add the CPT code, modifier, and responsibility to the list.
             5. Click 'Calculate Total Responsibility' to see the total patient responsibility amount.
-            6. The entered data and the MDFS data will be displayed below.
+            6. The entered data will be displayed below the inputs.
         """)
 
-    # Layout the app with input fields on the left and data on the bottom
+    # Layout the app with input fields on the left and data on the right
     col1, col2 = st.columns(2)
     with col1:
         # Dynamic autocomplete for CPT code
         all_cpt_codes = data['CPT'].astype(str).unique().tolist()
-        typed_cpt_code = st.text_input('Enter CPT Code')
+        typed_cpt_code = st.text_input('Start typing CPT Code')
         filtered_cpt_codes = [code for code in all_cpt_codes if typed_cpt_code in code]
-        cpt_code = st.selectbox('Select CPT Code', filtered_cpt_codes) if typed_cpt_code else None
+        cpt_code = st.selectbox('Select CPT Code', filtered_cpt_codes) if filtered_cpt_codes else None
 
         # Get unique modifiers for the selected CPT code
         modifiers = data[data['CPT'] == cpt_code]['Modifier'].unique().tolist() if cpt_code in data['CPT'].values else []
@@ -158,37 +158,40 @@ else:
                 st.session_state.modifiers = []
                 st.session_state.responsibilities = []
 
-    # Display the calculated tables at the bottom
-    if len(st.session_state.cpt_codes) == len(st.session_state.modifiers) == len(st.session_state.responsibilities):
-        if st.session_state.cpt_codes:
-            input_data = pd.DataFrame({
-                'CPT Code': st.session_state.cpt_codes,
-                'Modifier': st.session_state.modifiers,
-                '% Patient Responsibility': st.session_state.responsibilities
-            })
+    # Display the calculated Patient Responsibility Table in the right column
+    with col2:
+        if len(st.session_state.cpt_codes) == len(st.session_state.modifiers) == len(st.session_state.responsibilities):
+            if st.session_state.cpt_codes:
+                input_data = pd.DataFrame({
+                    'CPT Code': st.session_state.cpt_codes,
+                    'Modifier': st.session_state.modifiers,
+                    '% Patient Responsibility': st.session_state.responsibilities
+                })
 
-            # Merge input data with MDFS data to get 'Non Facility Fee'
-            merged_input_data = input_data.merge(data[['CPT', 'Modifier', 'Non Facility Fee']],
-                                                 left_on=['CPT Code', 'Modifier'],
-                                                 right_on=['CPT', 'Modifier'],
-                                                 how='left')
-            merged_input_data['% pts'] = merged_input_data['% Patient Responsibility'].astype(str) + '%'
-            merged_input_data.set_index(['CPT Code', 'Modifier'], inplace=True)
+                # Merge input data with MDFS data to get 'Non Facility Fee'
+                merged_input_data = input_data.merge(data[['CPT', 'Modifier', 'Non Facility Fee']],
+                                                     left_on=['CPT Code', 'Modifier'],
+                                                     right_on=['CPT', 'Modifier'],
+                                                     how='left')
+                merged_input_data['% pts'] = merged_input_data['% Patient Responsibility'].astype(str) + '%'
+                merged_input_data.set_index(['CPT Code', 'Modifier'], inplace=True)
 
-            # Calculate total responsibility
-            merged_data = input_data.merge(data, left_on=['CPT Code', 'Modifier'], right_on=['CPT', 'Modifier'])
-            merged_data['Patient Responsibility'] = (merged_data['% Patient Responsibility'] / 100) * merged_data['Non Facility Fee']
-            total_responsibility = merged_data['Patient Responsibility'].sum()
+                st.write('### Patient Responsibility Table')
+                st.write(merged_input_data[['% pts', 'Non Facility Fee']])
 
-            if st.button('Calculate Total Responsibility'):
-                st.write(f'### Total Patient Responsibility: ${total_responsibility:.2f}')
+                # Calculate total responsibility
+                merged_data = input_data.merge(data, left_on=['CPT Code', 'Modifier'], right_on=['CPT', 'Modifier'])
+                merged_data['Patient Responsibility'] = (merged_data['% Patient Responsibility'] / 100) * merged_data['Non Facility Fee']
+                total_responsibility = merged_data['Patient Responsibility'].sum()
 
-            # Display the tables
-            st.write('### Patient Responsibility Table')
-            st.write(merged_input_data[['% pts', 'Non Facility Fee']])
+                if st.button('Calculate Total Responsibility'):
+                    st.write(f'### Total Patient Responsibility: ${total_responsibility:.2f}')
 
-            st.write('### MDFS Data for Selected CPT Codes and Modifiers')
-            merged_data.set_index(['CPT Code', 'Modifier'], inplace=True)
-            st.write(merged_data)
+    # Display the MDFS Data Table at the bottom
+    if st.session_state.cpt_codes:
+        st.write('### MDFS Data for Selected CPT Codes and Modifiers')
+        merged_data.set_index(['CPT Code', 'Modifier'], inplace=True)
+        st.write(merged_data)
+
 
  
